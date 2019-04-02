ba3e8edcebaa4440efa88859a8937248b2a1c353
Jean-Jacques Hiblot <jjhiblot@ti.com>
2019-04-02 12:28:28 +0200

    phy: keystone-usb: handle the transition of the USB power domain
    There is no proper power domain support for the keystone platforms.
    However we need to turn off the USB domains before jumping to linux or it
    fail to boot (observed with k2e and k2l platforms).
    This can be done in the PHY driver as it is dedicated only to the keystone
    platforms and matches the required on/off sequence.
    
    Signed-off-by: Jean-Jacques Hiblot <jjhiblot@ti.com>

--- drivers/phy/keystone-usb-phy.c
+++ drivers/phy/keystone-usb-phy.c
@@ -24,14 +24,24 @@
 #define PHY_REF_SSP_EN			BIT(29)
 
 struct keystone_usb_phy {
+	u32 psc_domain;
 	void __iomem *reg;
 };
 
 static int keystone_usb_init(struct phy *phy)
 {
 	u32 val;
+	int rc;
 	struct udevice *dev = phy->dev;
 	struct keystone_usb_phy *keystone = dev_get_priv(dev);
+
+	/* Release USB from reset */
+	rc = psc_enable_module(keystone->psc_domain);
+	if (rc) {
+		debug("Cannot enable USB module");
+		return -rc;
+	}
+	mdelay(10);
 
 	/*
 	 * VBUSVLDEXTSEL has a default value of 1 in BootCfg but shouldn't.
@@ -74,12 +84,23 @@ static int keystone_usb_power_off(struct phy *phy)
 
 static int keystone_usb_exit(struct phy *phy)
 {
+	struct udevice *dev = phy->dev;
+	struct keystone_usb_phy *keystone = dev_get_priv(dev);
+
+	if (psc_disable_module(keystone->psc_domain))
+		debug("failed to disable USB module!\n");
+
 	return 0;
 }
 
 static int keystone_usb_phy_probe(struct udevice *dev)
 {
+	int rc;
 	struct keystone_usb_phy *keystone = dev_get_priv(dev);
+
+	rc = dev_read_u32(dev, "psc-domain", &keystone->psc_domain);
+	if (rc)
+		return rc;
 
 	keystone->reg = dev_remap_addr_index(dev, 0);
 	if (!keystone->reg) {
