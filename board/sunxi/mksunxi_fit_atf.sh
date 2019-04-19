21c828b7901c8f19e8783243f28f0d32e7874c49
Jean-Jacques Hiblot <jjhiblot@ti.com>
2019-04-19 14:29:48 +0200

    Makefile: Query the SPL Fit Generator for its dependencies
    To reduce the complexity of the Makefile, let the generator tell what its
    dependencies are. For this purpose use the "--deps" option.
    
    Signed-off-by: Jean-Jacques Hiblot <jjhiblot@ti.com>

--- board/sunxi/mksunxi_fit_atf.sh
+++ board/sunxi/mksunxi_fit_atf.sh
@@ -5,6 +5,11 @@
 #
 # usage: $0 <dt_name> [<dt_name> [<dt_name] ...]
 
+if [ x"$1" = x"--deps" ]; then
+	echo "u-boot-nodtb.bin"
+	exit 0
+fi
+
 [ -z "$BL31" ] && BL31="bl31.bin"
 
 if [ ! -f $BL31 ]; then
